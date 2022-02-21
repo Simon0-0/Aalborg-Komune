ab266da572acfa40d8358f4507a457979d8c1091
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2022-02-21 22:05:14 +0100

    asd

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 const menu = document.getElementById("menu");
 const ul = document.getElementById("ul");
+const text = document.getElementById("headerText")
 
 menu.addEventListener('click', () => {
     ul.classList.toggle("show");
+    text.classList.toggle("show")
 });
