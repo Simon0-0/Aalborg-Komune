53a4906b92a10a6db3e194354018431f3a831371
Simon0-0 <97345020+Simon0-0@users.noreply.github.com>
2022-02-22 02:09:20 +0100

    walking ra\ndom
    on

--- assets/app.js
+++ assets/app.js
@@ -6,3 +6,12 @@ menu.addEventListener('click', () => {
     ul.classList.toggle("show");
     text.classList.toggle("show")
 });
+
+
+
+document.getElementById('walkArray').addEventListener('click', function () {
+    let ExtremeArray = ['path 1', 'path 2', 'path 3', 'path 4', 'path 5'];
+    const randomItem = ExtremeArray[Math.floor(Math.random()*ExtremeArray.length)];
+    document.getElementById('RandomArray').innerHTML = randomItem;
+
+});    
